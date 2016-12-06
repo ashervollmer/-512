de4ba6e73e0728f6ef6b673cb743b065c36f3aeb
ashervollmer <begerreger@gmail.com>
2016-12-06 20:41:24 +0000

    common 64s

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? Math.random() < 0.9 ? 1 : 2 : Math.random() < 0.99 ? 1 : 64;
+    var value = Math.random() < 0.8 ? Math.random() < 0.9 ? 1 : 2 : Math.random() < 0.9 ? 1 : 64;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
